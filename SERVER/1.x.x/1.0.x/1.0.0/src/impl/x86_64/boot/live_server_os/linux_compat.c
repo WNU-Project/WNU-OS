59f1692f756ad75c22fd0f5f7891460c2187e652
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-11-15 16:11:15 -0500

    refactor: Remove comment about text color in Linux application loader

--- SERVER/1.x.x/1.0.x/1.0.0/src/impl/x86_64/boot/live_server_os/linux_compat.c
+++ SERVER/1.x.x/1.0.x/1.0.0/src/impl/x86_64/boot/live_server_os/linux_compat.c
@@ -6,7 +6,7 @@
 
 // Linux application loader using safe printf
 int load_linux_app(const char* app_name) {
-    vga(80, 7, "\x4C"); // L - White text on blue background
+    vga(80, 7, "\x4C"); // L
     vga(81, 7, "\x49"); // I
     vga(82, 7, "\x4E"); // N
     vga(83, 7, "\x55"); // U
