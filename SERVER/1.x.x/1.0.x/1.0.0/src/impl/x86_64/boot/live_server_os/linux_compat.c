0ff6e40858611a4ea274d2a713c53a19994891c7
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-11-15 16:11:55 -0500

    refactor: Update comment to clarify Linux application loader implementation using vga() function

--- SERVER/1.x.x/1.0.x/1.0.0/src/impl/x86_64/boot/live_server_os/linux_compat.c
+++ SERVER/1.x.x/1.0.x/1.0.0/src/impl/x86_64/boot/live_server_os/linux_compat.c
@@ -4,7 +4,7 @@
 
 #include "vga.h"
 
-// Linux application loader using safe printf
+// Linux application loader using vga() function
 int load_linux_app(const char* app_name) {
     vga(80, 7, "\x4C"); // L
     vga(81, 7, "\x49"); // I
