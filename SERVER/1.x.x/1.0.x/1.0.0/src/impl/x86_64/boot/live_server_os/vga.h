941df750c09c07eab47ef9db7263608ce55c34a6
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-11-15 23:29:30 -0500

    feat: Add definitions for VGA colors and letters in vga.h

--- SERVER/1.x.x/1.0.x/1.0.0/src/impl/x86_64/boot/live_server_os/vga.h
+++ SERVER/1.x.x/1.0.x/1.0.0/src/impl/x86_64/boot/live_server_os/vga.h
@@ -7,4 +7,25 @@ int vga_print(int char_count, int color, char* textasciivaule);
 // Clear screen function
 void clear_screen();
 
+// Define VGA Colors
+#define VGA_COLOR_BLACK         0
+#define VGA_COLOR_BLUE          1
+#define VGA_COLOR_GREEN         2
+#define VGA_COLOR_CYAN          3
+#define VGA_COLOR_RED           4
+#define VGA_COLOR_MAGENTA       5
+#define VGA_COLOR_BROWN         6
+#define VGA_COLOR_WHITE         7
+#define VGA_COLOR_GRAY          8
+#define VGA_COLOR_LIGHT_BLUE    9
+#define VGA_COLOR_LIGHT_GREEN   10
+#define VGA_COLOR_LIGHT_CYAN    11
+#define VGA_COLOR_LIGHT_RED     12
+#define VGA_COLOR_LIGHT_MAGENTA 13
+#define VGA_COLOR_YELLOW        14
+#define VGA_COLOR_BRIGHT_WHITE  15
+
+// Define Letters
+#define VGA_LETTER_A "\x41"
+
 #endif /* VGA_H */
